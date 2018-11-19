5f9275696bb9d1b7b7a5a89e0e717ed1a46bf69e
CRISTINAJAVAAWS <c.barrigaradiologia@gmail.com>
2018-11-19 11:39:37 +0100

    update

---
+++ testmaven/src/main/java/com/CristinaCompany/testmaven/App.java
@@ -0,0 +1,13 @@
+package com.CristinaCompany.testmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
